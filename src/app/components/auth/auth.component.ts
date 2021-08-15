930105a4be65edea655a12e04e35b14ca7634cca
Dylan Baird <Baird.Dylan@gmail.com>
2021-08-14 20:46:10 -0400

    Now saving auth user and authenticated boolean in local storage

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,7 +1,11 @@
+// ANGULAR
 import { Component, OnInit } from '@angular/core';
+
+// RABBIT CHAT
 import {HttpService} from '../../services/http.service';
 import {SimpleSignInRequest} from '../../models/requests/simple-sign-in-request';
 import {RabbitUser} from '../../models/rabbit-user';
+import {DataService} from '../../services/data.service';
 
 @Component({
     selector: 'app-auth',
@@ -9,15 +13,31 @@ import {RabbitUser} from '../../models/rabbit-user';
     styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+    /*
+    Hardcoded for testing purposes.
+     */
     private username: string = 'firstuser';
     private password: string = 'demo';
 
-    constructor(private httpService: HttpService) { }
+    constructor(private httpService: HttpService,
+                private dataService: DataService) { }
 
     ngOnInit(): void { }
 
     public handleSignInRequest(): void {
         const simpleSignInRequest: SimpleSignInRequest = { username: this.username, password: this.password };
-        this.httpService.postSimpleSignInRequest(simpleSignInRequest).subscribe((user: RabbitUser) => { console.log(user); });
+
+        this.httpService.postSimpleSignInRequest(simpleSignInRequest).subscribe((user: RabbitUser) => {
+            console.log(user);
+            this.dataService.setObject('AUTHUSER', user);
+
+            const test: RabbitUser = this.dataService.getObject('AUTHUSER');
+            console.log(test);
+
+            this.dataService.setBoolean('AUTHENTICATED', true);
+
+            const authTest: boolean = this.dataService.getBoolean('AUTHENTICATED');
+            console.log(authTest);
+        });
     }
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,7 +1,11 @@
 // ANGULAR
 import { HttpClient } from '@angular/common/http';
 import {Injectable} from '@angular/core';
+
+// OTHER
 import {Observable} from 'rxjs';
+
+// RABBIT CHAT
 import { Config } from '../config/env.config';
 import {CreateMessageRequest} from '../models/requests/create-message-request';
 import {SimpleSignInRequest} from '../models/requests/simple-sign-in-request';
@@ -19,6 +23,9 @@ export class HttpService {
         return this.httpClient.post(`${Config.apiUrl}/message`, createMessageRequest);
     }
 
+    /*
+    TODO: The method is called "post..." but it is a put request. Resolve the discrepancy.
+     */
     public postSimpleSignInRequest(simpleSignInRequest: SimpleSignInRequest): Observable<any> {
         return this.httpClient.put(`${Config.apiUrl}/auth`, simpleSignInRequest);
     }
